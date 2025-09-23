a15f3b6e466b333446ba4035a8b3db61e70cef94
kimuratakuou <ruia1082halfnc@gmail.com>
2025-09-23 16:19:28 +0900

    TodoListAdapterリファクタリング

--- app/src/main/java/com/takaobrog/androidapiapp/presentation/todo/todo_list/TodoListAdapter.kt
+++ app/src/main/java/com/takaobrog/androidapiapp/presentation/todo/todo_list/TodoListAdapter.kt
@@ -41,7 +41,7 @@ class TodoListAdapter : ListAdapter<TodoUiModel, TodoListAdapter.VH>(DIFF) {
     override fun onBindViewHolder(holder: VH, position: Int) {
         val todo = getItem(position)
 
-        with(holder.binding) {
+        holder.binding.apply {
             todoTitle.text = todo.title
             datetime.text = todo.datetime
 
@@ -49,17 +49,19 @@ class TodoListAdapter : ListAdapter<TodoUiModel, TodoListAdapter.VH>(DIFF) {
             val colorInt = ContextCompat.getColor(root.context, colorRes)
             root.setBackgroundColor(colorInt)
 
-            isDone.setOnCheckedChangeListener(null)
-            isDone.isChecked = todo.done
-            isDone.setOnCheckedChangeListener { _, isChecked ->
-                if (holder.adapterPosition != RecyclerView.NO_POSITION) {
-                    isDoneCheckListener?.onItemCheck(todo.id, isChecked)
-                }
-            }
-
             root.setOnClickListener {
                 if (holder.adapterPosition != RecyclerView.NO_POSITION) {
                     listener?.onItemClick(todo)
+                }
+            }
+        }
+
+        holder.binding.isDone.apply {
+            setOnCheckedChangeListener(null)
+            isChecked = todo.done
+            setOnCheckedChangeListener { _, isChecked ->
+                if (holder.adapterPosition != RecyclerView.NO_POSITION) {
+                    isDoneCheckListener?.onItemCheck(todo.id, isChecked)
                 }
             }
         }
