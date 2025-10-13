8ef83a8a4807948f08d0b93964129715a5afcd5d
kimuratakuou <ruia1082halfnc@gmail.com>
2025-10-13 10:49:09 +0900

    core不要ファイル削除

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,5 +11,4 @@ buildscript {
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
 }
